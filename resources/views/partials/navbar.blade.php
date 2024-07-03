40f0cef4bbf2c475a080a1fad64acbb3ee7caad9
dickybulin26 <havidzgojek@gmail.com>
2024-07-03 21:50:36 +0700

    membuat user login & middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,6 +19,38 @@
             <a class="nav-link {{ ($active === "categories" ? 'active' : '') }}" href="/categories">categories</a>
           </li>
         </ul>
+
+        <ul class="navbar-nav mx-auto">
+          @auth
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown"
+                  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                welcome back, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" class="dropdown-item" href="/dashboard" style="text-decoration: none">
+                  <i class="bi bi-layout-text-window-reverse"></i> Dashboard</a>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item">
+                      <a href="" class="dropdown-item" class="dropdown-item"
+                            style="text-decoration: none"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+                    </button>
+                  </form>
+
+              </ul>
+            </li>
+          @else
+            <li class="nav-item">
+              <a href="/login" class="nav-link {{ ($active === "login" ? 'active' : '') }}">
+                <i class="bi bi-box-arrow-in-right"></i>Login</a>
+            </li>
+          @endauth
+        </ul>
+
       </div>
     </div>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,19 +48,11 @@ Route::get('/categories', function () {
     ]);
 });
 
-// Route::get('/categories/{category:slug}', function (Category $category) {
-//     return view('blog', [
-//         'title' => "Post by category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category','author'),
-//     ]);
-// });
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'auth']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
 
-// Route::get('/authors/{Author:username}', function (User $Author) {
-//     return view('blog', [
-//         'title' => "User by Athors : $Author->name",
-//         'active' => 'posts',
-//         'posts' => $Author->posts->load('category', 'author'),
-//     ]);
-// });
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
